8eb13ab2b9a39d6363eeb832ffc58cb505dc3a25
39312657 <tacjka34@gmail.com>
2020-05-20 17:38:39 +0300

    fixed register
    '

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,24 +1,33 @@
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
+const smtpPool = require('nodemailer/lib/smtp-pool');
 const sendEmail = async (message,callback) => {
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.GMAIL_USER,
-            pass: process.env.GMAIL_PASS,
-        },
+    const transporter = nodemailer.createTransport(new smtpPool({
+        host: "smtp-mail.outlook.com", 
+        secureConnection: false, 
         tls: {
-            rejectUnauthorized: false
-        }
-    });
-    await transporter.sendMail(message, (err, info) => {
+           ciphers:'SSLv3'
+        },
+        auth: {
+            user: "exzzemple.sender@outlook.com",
+            pass: `i"+Mv!;8vV<PN9t`,
+        },
+    }));
+
+    transporter.sendMail(message, (err, info) => {
+        console.log(message)
+        
         if (err) {
-            return err;
+            console.log('Err sent: ' + JSON.stringify(err));
+            return err; 
         }
+        
+        console.log('Email sent: ' + info.response);
         callback()
         transporter.close();
     });
+
 }
 
 module.exports = sendEmail;
