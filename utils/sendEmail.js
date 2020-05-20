8ee8c4e4abdd8006a9164b9d2dd59f3ace634f7e
bandvov <bandvov@gmail.com>
2020-05-20 10:39:34 +0300

    fixed nodemailer

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,11 +3,15 @@ require('dotenv').config();
 
 const sendEmail = async (message,callback) => {
     const transporter = nodemailer.createTransport({
-        service: 'gmail',
+        service: 'Gmail',
+        port: 587,
         auth: {
             user: process.env.GMAIL_USER,
             pass: process.env.GMAIL_PASS,
         },
+        tls: {
+            rejectUnauthorized: false
+        }
     });
     await transporter.sendMail(message, (err, info) => {
         if (err) {
