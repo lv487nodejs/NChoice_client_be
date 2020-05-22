db316a82b31886d361a70af1b12cc89f3212babb
Oleg Reekee <reekee94@gmail.com>
2020-05-22 21:52:41 +0300

    'fix'

--- routes/users/auth.js
+++ routes/users/auth.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const { userLoginValidationRules, validate } = require('../../middleware/validator');
+const passport = require('passport')
+require('../../config/passport-config')
+
+const passportGoogleIndus = passport.authenticate('googleToken', {session: false})
+const passportFacebookIndus = passport.authenticate('facebookToken', {session: false})
 
 const {
     loginUser,
@@ -7,12 +12,18 @@ const {
     getToken,
     logout,
     emailConfirmation,
+    googleAuth,
+    facebookAuth,
 } = require('../../controllers/users/auth')
 
 const router = express.Router();
 
 // user login
 router.post('/login', userLoginValidationRules(), validate, loginUser);
+
+router.post('/oauth/google', passportGoogleIndus, googleAuth)
+
+router.post('/oauth/facebook', passportFacebookIndus, facebookAuth)
 
 router.post('/admin/login', loginAdmin);
 
