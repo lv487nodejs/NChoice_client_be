03cd7d9746461ca9fb5209cb18cdb29bddbecf80
bandvov <bandvov@gmail.com>
2020-05-23 10:25:07 +0300

    updated ratings backend

--- controllers/rating/rating.js
+++ controllers/rating/rating.js
@@ -5,11 +5,13 @@ const ErrorResponse = require('../../utils/errorResponse');
 const updateRating = asyncHandler(async (req, res, next) => {
     const { id } = req.params;
     const { rate } = req.body;
+    
     const productToUpdate = await Product.findById(id);
     if (!productToUpdate) {
         return next(new ErrorResponse('Product not found', 404));
     }
-    productToUpdate.rate = rate;
+    productToUpdate.rateCount = ++productToUpdate.rateCount; 
+    productToUpdate.rate = (((productToUpdate.rate * productToUpdate.rateCount)+ +rate) /productToUpdate.rateCount).toFixed(0);
     productToUpdate.save();
     res.status(200).send('rating updated');
 })
