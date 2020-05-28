4777b591cdc5cafceb81c33b53fd47cdf03ad728
bandvov <bandvov@gmail.com>
2020-05-28 10:06:52 +0300

    fixed. added prettierrc

--- controllers/rating/rating.js
+++ controllers/rating/rating.js
@@ -1,6 +1,9 @@
-const asyncHandler = require("../../middleware/async");
-const Product = require("../../models/Product");
-const ErrorResponse = require("../../utils/errorResponse");
+const asyncHandler = require('../../middleware/async');
+const Product = require('../../models/Product');
+const ErrorResponse = require('../../utils/errorResponse');
+
+const UPDATE_RATING_ERROR_MESSAGE = 'Product not found';
+const USER_VOTED_ERROR_MESSAGE = 'You already voted';
 
 const updateRating = asyncHandler(async (req, res, next) => {
   const { id } = req.params;
@@ -8,10 +11,10 @@ const updateRating = asyncHandler(async (req, res, next) => {
 
   const productToUpdate = await Product.findById(id);
   if (!productToUpdate) {
-    return next(new ErrorResponse("Product not found", 404));
+    return next(new ErrorResponse(UPDATE_RATING_ERROR_MESSAGE, 404));
   }
   if (productToUpdate.usersVoted.includes(userId)) {
-    return next(new ErrorResponse("You already voted", 400));
+    return next(new ErrorResponse(USER_VOTED_ERROR_MESSAGE, 400));
   }
   productToUpdate.rateCount = ++productToUpdate.rateCount;
   productToUpdate.rate = (
@@ -20,6 +23,6 @@ const updateRating = asyncHandler(async (req, res, next) => {
   ).toFixed(0);
   productToUpdate.usersVoted.push(userId);
   productToUpdate.save();
-  res.status(200).send("rating updated");
+  res.status(200).send('rating updated');
 });
 module.exports = { updateRating };
